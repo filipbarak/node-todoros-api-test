a4fe2a58570fd84490aa839d2f4bf7c6465061c5
Filip Barakovski <filipbarak94@gmail.com>
2018-01-07 12:31:30 +0100

    Adds GET with :id param

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todoro} = require('./models/todoro.js');
@@ -28,6 +29,27 @@ app.get('/todoros', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+// GET /todos/id
+
+app.get('/todoros/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send({
+            message: 'ID is not valid'
+        })
+    }
+    Todoro.findById(id).then((todoro) => {
+        if (!todoro) {
+            return res.status(404).send({
+                message: 'Todoro not found.'
+            });
+        }
+        res.send({todoro})
+    }, (e) => {
+        res.status(400).send()
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
