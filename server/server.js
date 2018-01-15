0fa22d77390b2c1b7203e11bdbb7c565f8acbe27
Filip Barakovski <filipbarak94@gmail.com>
2018-01-15 20:53:17 +0100

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -7,7 +9,7 @@ var {Todoro} = require('./models/todoro.js');
 var {User} = require('./models/user.js');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json())
 
