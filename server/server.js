558c251e1769d0649c0db11ec076be6540b32b48
Filip Barakovski <filipbarak94@gmail.com>
2018-01-15 21:03:53 +0100

    Preparing for Heroku Deploy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
-require('./config/config');
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoroApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoroAppTest';
+}
+
 
 var express = require('express');
 var bodyParser = require('body-parser');
