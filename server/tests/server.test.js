3f52fe3ff9f04ff20a82b126912935275abb55ec
filip.barakovski@marand.si <filipbarak94@gmail.com>
2018-01-23 15:18:39 +0100

    Adds test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -252,4 +252,24 @@ describe('POST /users/login', () => {
             })
             .end(done);
     })
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should delete the token', (done) => {
+        let token = users[0].tokens[0].token;
+        let id = users[0]._id;
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+         })
+    })
 })
